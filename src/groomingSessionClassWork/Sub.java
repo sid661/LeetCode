f0c54a56cd10497fe7eee1984d1adb22ee5ec969
Sidharth Mishra <mishrasidharth83@gmail.com>
2023-03-28 14:41:49 +0530

    new array

--- src/groomingSessionClassWork/Sub.java
+++ src/groomingSessionClassWork/Sub.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class Sub {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 4};
+        int[] arr = new int[]{1, 2, 3, 4,5,6,7,8,9};
         containsDuplicate(arr);
     }
 
